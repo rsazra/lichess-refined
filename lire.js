a506c63cbb3a09a2bb3f69bea2cef0d6b9b264c5
rsazra <rajbir.singh.azra@gmail.com>
2024-08-16 01:45:50 -0400

    redo layout, start fun stuff

--- lire.js
+++ lire.js
@@ -1,16 +1,42 @@
 document.body.style.border = "5px solid blue";
 
 // HTML changes
-const toDelete = document.querySelectorAll('.lobby__tv, .lobby__blog, .lobby__leaderboard, .lobby__winners, .lobby__tournaments-simuls, .lobby__support');
-toDelete.forEach(element => {
-  element.remove();
-});
+const toDelete = document.querySelectorAll('.lobby__tv, .lobby__blog,\
+  .lobby__leaderboard, .lobby__winners, .lobby__tournaments-simuls, \
+  .lobby__support, .lobby__counters, .lobby__streams, .lobby__timeline');
+toDelete.forEach(element => { element.remove() });
+//move puzzle into a div
+// kind of bugged. should check if this has already been done.
+const puzzleAnchor = document.querySelector('.lobby__puzzle');
+if (puzzleAnchor.tagName == "A") {
+  const puzzleDiv = document.createElement('div');
+  puzzleAnchor.parentNode.insertBefore(puzzleDiv, puzzleAnchor);
+  puzzleDiv.appendChild(puzzleAnchor);
+  puzzleAnchor.classList.add('puzzle__anchor');
+  puzzleAnchor.classList.remove('lobby__puzzle');
+  puzzleDiv.classList.add('lobby__puzzle');
+}
+
+// add puzzle rating
+// requires authorized request. will handle that later.
+const puzzleRating = puzzleAnchor.firstElementChild.firstElementChild;
+// why is this null at first?
+if (puzzleRating) {
+  console.log("firstchild", puzzleRating);
+  puzzleRating.innerHTML = `Puzzle rating: ${2000}`;
+}
 
 
 // new sidebar component with ratings and streaks (linking to profile page)
-
-async function getRating(username, perf) {
-  const url = `https://lichess.org/api/user/${username}/perf/${perf}`;
+const lobbyRatings = document.querySelector('.lobby__spotlights, .lobby__ratings');
+if (lobbyRatings.classList.contains("lobby__spotlights")) {
+  lobbyRatings.classList.remove('lobby__spotlights');
+  lobbyRatings.classList.add('lobby__ratings');
+}
+lobbyRatings.childNodes.forEach(element => { element.remove() })
+// getting sidebar data
+async function getRating(username, format) {
+  const url = `https://lichess.org/api/user/${username}/perf/${format}`;
 
   try {
     const response = await fetch(url);
@@ -23,10 +49,10 @@ async function getRating(username, perf) {
 
     if (data.perf) {
       const rating = data.perf.glicko.rating;
-      console.log(`${username}'s ${perf} rating is: ${rating}`);
+      console.log(`${username}'s ${format} rating is: ${rating}`);
       return rating;
     } else {
-      console.log(`No rating found for ${username} in ${perf}`);
+      console.log(`No rating found for ${username} in ${format}`);
       return null;
     }
   } catch (error) {
@@ -34,21 +60,13 @@ async function getRating(username, perf) {
     return null;
   }
 }
-
 const username = 'daphnebridgerton';
 const formats = ['bullet', 'blitz', 'rapid', 'classical'];
 // corresponding numbers for search: 1, 2, 6, 3. dict or list?
 getRating(username, formats[1]);
-
-
-
-// link to (https://lichess.org/) @/${username}/search?perf={index of perf in formats)}
-// experiment in adding font manually, but probably not needed
-// const font = new FontFace("licon", "url('lichess.ttf')");
-// document.fonts.add(font);
-
-// const icon = '';
-// const textNode = document.createTextNode('');
-// textNode.fontFamily = 'lichess';
-// document.body.append(textNode)
-
+// add to widget
+const bulletRating = document.createElement("a");
+bulletRating.href = `https://lichess.org/@/${username}/perf/bullet`
+bulletRating.classList.add("little", "tour-spotlight")
+bulletRating.innerHTML = `<i data-icon="" class="img"></i><span class="content"><span class="name">Current rating</span><span class="more">Current streak</span></span>`
+lobbyRatings.appendChild(bulletRating);
